b5f671c8b1829640ac1c6d7bf52bae8c0d9a7ca6
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-08-11 23:59:06 -0300

    setup: add first dependencies

---
+++ src/main/java/com/devworks/cloudcommerce/CloudCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devworks.cloudcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devworks/cloudcommerce/CloudCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devworks.cloudcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
