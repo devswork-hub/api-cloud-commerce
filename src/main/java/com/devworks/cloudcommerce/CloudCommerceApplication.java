a9ee3accd812660ae05509bf94b76ca59a9b12c4
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-08-17 15:14:36 -0300

    feat: add a generic builder

--- src/main/java/com/devworks/cloudcommerce/CloudCommerceApplication.java
+++ src/main/java/com/devworks/cloudcommerce/CloudCommerceApplication.java
@@ -11,12 +11,6 @@ public class CloudCommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudCommerceApplication.class, args);
-
-		var test = GenericBuilder.of(User::new)
-			.with(User::setPhoneNumber, "123")
-			.build();
-
-		System.out.println(test.getPhoneNumber());
 	}
 
 }
