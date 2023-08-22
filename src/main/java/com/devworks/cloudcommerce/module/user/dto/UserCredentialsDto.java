ff20b9fe96c60f644609742819257a4b7cf61a8c
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-08-22 16:33:25 -0300

    feat: add dto and mapper to role entity

---
+++ src/main/java/com/devworks/cloudcommerce/module/user/dto/UserCredentialsDto.java
@@ -0,0 +1,36 @@
+package com.devworks.cloudcommerce.module.user.dto;
+
+import com.devworks.cloudcommerce.module.user.model.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserCredentialsDto {
+    private UUID id;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime updatedAt;
+
+    @NotEmpty
+    @Email
+    private String email;
+
+    @NotEmpty
+    @Size(min = 8, message = "username should have at least 8 characters")
+    private String username;
+
+    @NotEmpty
+    @Size(min = 8, message = "password should have at least 8 characters")
+    private String password;
+
+    private List<Role> roles;
+}
---
+++ src/main/java/com/devworks/cloudcommerce/module/user/mapper/UserCredentialsMapper.java
@@ -0,0 +1,36 @@
+package com.devworks.cloudcommerce.module.user.mapper;
+
+import com.devworks.cloudcommerce.module.user.dto.UserCredentialsDto;
+import com.devworks.cloudcommerce.module.user.model.UserCredentials;
+
+public class UserCredentialsMapper {
+    private UserCredentialsMapper() {
+        throw new IllegalStateException("You cannot instantiate a utility class");
+    }
+
+    public static UserCredentials toEntity(UserCredentialsDto dto) {
+        return UserCredentials.builder()
+            .id(dto.getId())
+            .createdAt(dto.getCreatedAt())
+            .updatedAt(dto.getUpdatedAt())
+
+            .email(dto.getEmail())
+            .username(dto.getUsername())
+            .password(dto.getPassword())
+            .roles(dto.getRoles())
+            .build();
+    }
+
+    public static UserCredentialsDto toDto(UserCredentials entity) {
+        return UserCredentialsDto.builder()
+            .id(entity.getId())
+            .createdAt(entity.getCreatedAt())
+            .updatedAt(entity.getUpdatedAt())
+
+            .email(entity.getEmail())
+            .username(entity.getUsername())
+            .password(entity.getPassword())
+            .roles(entity.getRoles())
+            .build();
+    }
+}
