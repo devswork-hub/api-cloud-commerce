e691910150670ec61a9865e1e734bae5b0628790
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-08-22 14:37:02 -0300

    feat: create role entity

---
+++ src/main/java/com/devworks/cloudcommerce/module/user/model/Role.java
@@ -0,0 +1,22 @@
+package com.devworks.cloudcommerce.module.user.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id", nullable = false)
+    private Long id;
+    private String name;
+    private String description;
+}
