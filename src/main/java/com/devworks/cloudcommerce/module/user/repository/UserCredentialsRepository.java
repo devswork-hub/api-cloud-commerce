bf900b2088a7075a45e8ff99b175f42319ceb1a8
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-08-22 14:37:41 -0300

    feat: create user credentials repostory

---
+++ src/main/java/com/devworks/cloudcommerce/module/user/repository/UserCredentialsRepository.java
@@ -0,0 +1,12 @@
+package com.devworks.cloudcommerce.module.user.repository;
+
+import com.devworks.cloudcommerce.module.user.model.UserCredentials;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserCredentialsRepository extends JpaRepository<UserCredentials, Long> {
+    Optional<UserCredentials> findByEmail(String email);
+}
