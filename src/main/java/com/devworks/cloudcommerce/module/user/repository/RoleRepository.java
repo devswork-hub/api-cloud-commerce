2ffaf145f32e0511b6de9c7b1c880f046c352f4d
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-08-26 22:55:57 -0300

    changes: add custom query to verify role name is exists

---
+++ src/main/java/com/devworks/cloudcommerce/module/user/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.devworks.cloudcommerce.module.user.repository;
+
+import com.devworks.cloudcommerce.module.user.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    @Query("SELECT r FROM Role r WHERE LOWER(r.name) = LOWER(?1)")
+    Optional<Role> findByName(String name);
+}
