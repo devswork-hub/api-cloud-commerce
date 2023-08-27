d8b413762982b6a77a5524af12f21ff84d09869e
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-08-26 22:55:34 -0300

    changes: add role dto and mapper

---
+++ src/main/java/com/devworks/cloudcommerce/module/user/mapper/RoleMapper.java
@@ -0,0 +1,32 @@
+package com.devworks.cloudcommerce.module.user.mapper;
+
+import com.devworks.cloudcommerce.module.user.dto.RoleDto;
+import com.devworks.cloudcommerce.module.user.model.Role;
+
+public class RoleMapper {
+    private RoleMapper() {
+        throw new IllegalStateException("You cannot instantiate a utility class");
+    }
+
+    public static Role toEntity(RoleDto dto) {
+        return Role.builder()
+            .id(dto.getId())
+            .createdAt(dto.getCreatedAt())
+            .updatedAt(dto.getUpdatedAt())
+
+            .name(dto.getName())
+            .description(dto.getDescription())
+            .build();
+    }
+
+    public static RoleDto toDto(Role entity) {
+        return RoleDto.builder()
+            .id(entity.getId())
+            .createdAt(entity.getCreatedAt())
+            .updatedAt(entity.getUpdatedAt())
+
+            .name(entity.getName())
+            .description(entity.getDescription())
+            .build();
+    }
+}
