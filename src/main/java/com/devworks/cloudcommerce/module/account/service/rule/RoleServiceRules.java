455c42bde17b187564ce83522aaf9cd6c9a1f04e
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-08-29 18:02:43 -0300

    change: update service rules

--- src/main/java/com/devworks/cloudcommerce/module/account/service/rule/RoleServiceRules.java
+++ src/main/java/com/devworks/cloudcommerce/module/account/service/rule/RoleServiceRules.java
@@ -3,10 +3,11 @@ package com.devworks.cloudcommerce.module.account.service.rule;
 import com.devworks.cloudcommerce.module.account.model.Role;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface RoleServiceRules {
     Role create(Role request);
     List<Role> findAll();
-    Role findById(Long id);
-    void delete(Long id);
+    Role findById(UUID id);
+    void delete(UUID id);
 }
