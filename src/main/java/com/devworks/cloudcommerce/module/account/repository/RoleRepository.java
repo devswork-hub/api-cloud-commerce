7cb956f35d22dbf6678ca637e76076326368df6f
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-08-29 18:03:24 -0300

    change: add UUID in role repository

--- src/main/java/com/devworks/cloudcommerce/module/account/repository/RoleRepository.java
+++ src/main/java/com/devworks/cloudcommerce/module/account/repository/RoleRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, UUID> {
     @Query("SELECT r FROM Role r WHERE LOWER(r.name) = LOWER(?1)")
     Optional<Role> findByName(String name);
 }
