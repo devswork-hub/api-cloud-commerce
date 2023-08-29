82c502ce13b218556cbd40cd7ed581b326391136
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-08-29 18:03:09 -0300

    change: check before create if is valid role

--- src/main/java/com/devworks/cloudcommerce/module/account/service/RoleService.java
+++ src/main/java/com/devworks/cloudcommerce/module/account/service/RoleService.java
@@ -2,12 +2,14 @@ package com.devworks.cloudcommerce.module.account.service;
 
 import com.devworks.cloudcommerce.common.exceptions.BadRequestException;
 import com.devworks.cloudcommerce.common.exceptions.NotFoundException;
+import com.devworks.cloudcommerce.module.account.constants.RolesTypes;
 import com.devworks.cloudcommerce.module.account.model.Role;
 import com.devworks.cloudcommerce.module.account.repository.RoleRepository;
 import com.devworks.cloudcommerce.module.account.service.rule.RoleServiceRules;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class RoleService implements RoleServiceRules {
@@ -17,13 +19,22 @@ public class RoleService implements RoleServiceRules {
         this.roleRepository = roleRepository;
     }
 
-
     @Override
     public Role create(Role request) {
         var existsRole = roleRepository.findByName(request.getName());
-
         if(existsRole.isPresent()) {
             throw new BadRequestException("Role has already been declared");
+        }
+
+        boolean validRoleType = false;
+        for (RolesTypes enumValue : RolesTypes.values()) {
+            if (enumValue.getName().equals(request.getName())) {
+                validRoleType = true;
+                break;
+            }
+        }
+        if (!validRoleType) {
+            throw new BadRequestException("Invalid role type");
         }
 
         return roleRepository.save(request);
@@ -35,13 +46,13 @@ public class RoleService implements RoleServiceRules {
     }
 
     @Override
-    public Role findById(Long id) {
+    public Role findById(UUID id) {
         return roleRepository.findById(id)
             .orElseThrow(() -> new NotFoundException("Not found role with id " + id));
     }
 
     @Override
-    public void delete(Long id) {
+    public void delete(UUID id) {
         findById(id);
         roleRepository.deleteById(id);
     }
