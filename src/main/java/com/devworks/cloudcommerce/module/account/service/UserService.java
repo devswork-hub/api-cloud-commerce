2c06f6e01f1c85c012bb6bc513d64fef34529818
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-08-29 18:00:53 -0300

    feat: assign default role to user

--- src/main/java/com/devworks/cloudcommerce/module/account/service/UserService.java
+++ src/main/java/com/devworks/cloudcommerce/module/account/service/UserService.java
@@ -1,35 +1,42 @@
 package com.devworks.cloudcommerce.module.account.service;
 
 import com.devworks.cloudcommerce.common.exceptions.BadRequestException;
+import com.devworks.cloudcommerce.module.account.constants.RolesTypes;
 import com.devworks.cloudcommerce.module.account.dto.UserDto;
 import com.devworks.cloudcommerce.module.account.mapper.UserMapper;
+import com.devworks.cloudcommerce.module.account.model.Role;
 import com.devworks.cloudcommerce.module.account.model.User;
+import com.devworks.cloudcommerce.module.account.repository.RoleRepository;
 import com.devworks.cloudcommerce.module.account.repository.UserRepository;
 import com.devworks.cloudcommerce.module.account.service.rule.UserServiceRules;
 import com.devworks.cloudcommerce.common.exceptions.NotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class UserService implements UserServiceRules {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Override
     public UserDto create(UserDto request) {
         var existsEmail = userRepository.findByEmail(request.getEmail());
-
-        if(existsEmail.isPresent()) {
+        if(existsEmail.isPresent())
             throw new BadRequestException("User with email already exists!");
-        }
 
-        var user = userRepository.save(UserMapper.toEntity(request));
-        return UserMapper.toDto(user);
+        var user = assignDefaultRoleToUser(UserMapper.toEntity(request));
+
+        var createdUser = userRepository.save(user);
+        return UserMapper.toDto(createdUser);
     }
 
     @Override
