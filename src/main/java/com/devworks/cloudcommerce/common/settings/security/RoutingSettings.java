2e15bfa3b7664c95d70319a4650e0c1b039b2b7a
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-09-01 09:51:47 -0300

    change: update routing setting

--- src/main/java/com/devworks/cloudcommerce/common/settings/security/RoutingSettings.java
+++ src/main/java/com/devworks/cloudcommerce/common/settings/security/RoutingSettings.java
@@ -1,5 +1,6 @@
 package com.devworks.cloudcommerce.common.settings.security;
 
+import com.devworks.cloudcommerce.common.security.filter.JwtFilter;
 import com.devworks.cloudcommerce.common.settings.routing.AccountModuleRouting;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,16 +8,34 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class RoutingSettings {
+    private final AccountModuleRouting accountModuleRouting;
+    private final JwtFilter jwtFilter;
+
+    public RoutingSettings(AccountModuleRouting accountModuleRouting, JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+        this.accountModuleRouting = accountModuleRouting;
+    }
+
     @Bean
     public SecurityFilterChain routingChain(HttpSecurity http) throws Exception {
-        http.apply(new AccountModuleRouting());
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.apply(accountModuleRouting).accountRoutingChains(http);
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+            .logout(l ->
+                l.logoutSuccessHandler(
+                    (request, response, authentication) -> SecurityContextHolder.clearContext()
+                )
+            );
+
         return http.build();
     }
 }
