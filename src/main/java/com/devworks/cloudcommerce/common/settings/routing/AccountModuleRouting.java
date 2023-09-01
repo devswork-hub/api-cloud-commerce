986e99e71977e8a7cd2baedd73904ebc4a4482c8
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-09-01 09:50:11 -0300

    change: update account module routing

--- src/main/java/com/devworks/cloudcommerce/common/settings/routing/AccountModuleRouting.java
+++ src/main/java/com/devworks/cloudcommerce/common/settings/routing/AccountModuleRouting.java
@@ -5,20 +5,23 @@ import com.devworks.cloudcommerce.module.account.routing.routes.UserRoute;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AccountModuleRouting extends AbstractHttpConfigurer<AccountModuleRouting, HttpSecurity> {
     public void accountRoutingChains(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-            .requestMatchers(HttpMethod.POST, UserRoute.DEFAULT.getValue()).permitAll()
-            .requestMatchers(HttpMethod.GET, UserRoute.DEFAULT.getValue()).permitAll()
-            .requestMatchers(HttpMethod.GET, UserRoute.USER_ALL_ROUTES_CHILD.getValue()).permitAll()
-            .requestMatchers(HttpMethod.DELETE, UserRoute.USER_ALL_ROUTES_CHILD.getValue()).permitAll()
+            .requestMatchers(HttpMethod.POST, "/sign-up").permitAll()
+            .requestMatchers(HttpMethod.POST, "/login").permitAll()
 
-            .requestMatchers(HttpMethod.POST, RoleRoute.DEFAULT.getValue()).permitAll()
-            .requestMatchers(HttpMethod.GET, RoleRoute.DEFAULT.getValue()).permitAll()
-            .requestMatchers(HttpMethod.GET, RoleRoute.ALL_ROUTES_CHILD.getValue()).permitAll()
-            .requestMatchers(HttpMethod.DELETE, RoleRoute.ALL_ROUTES_CHILD.getValue()).permitAll()
-            .requestMatchers(HttpMethod.POST, "/sign-up/").permitAll()
-            .requestMatchers(HttpMethod.POST, "/login").permitAll());
+            .requestMatchers(HttpMethod.GET, UserRoute.BASE.getValue()).authenticated()
+            .requestMatchers(HttpMethod.GET, UserRoute.ALL_CHILDREN.getValue()).authenticated()
+            .requestMatchers(HttpMethod.POST, UserRoute.BASE.getValue()).permitAll()
+            .requestMatchers(HttpMethod.DELETE, UserRoute.ALL_CHILDREN.getValue()).permitAll()
+
+            .requestMatchers(HttpMethod.POST, RoleRoute.BASE.getValue()).permitAll()
+            .requestMatchers(HttpMethod.GET, RoleRoute.BASE.getValue()).permitAll()
+            .requestMatchers(HttpMethod.GET, RoleRoute.ALL_CHILDREN.getValue()).permitAll()
+            .requestMatchers(HttpMethod.DELETE, RoleRoute.ALL_CHILDREN.getValue()).permitAll());
     }
 }
