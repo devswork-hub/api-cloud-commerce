96d7489cfd1b659078de55ec868336a7ed606cd7
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-09-01 09:51:29 -0300

    change: update custom details service

--- src/main/java/com/devworks/cloudcommerce/common/services/CustomUserDetailsService.java
+++ src/main/java/com/devworks/cloudcommerce/common/services/CustomUserDetailsService.java
@@ -1,8 +1,7 @@
 package com.devworks.cloudcommerce.common.services;
 
-import com.devworks.cloudcommerce.common.exceptions.NotFoundException;
-import com.devworks.cloudcommerce.module.account.repository.UserCredentialsRepository;
-import com.devworks.cloudcommerce.module.account.repository.UserRepository;
+import com.devworks.cloudcommerce.module.account.service.UserCredentialsService;
+import com.devworks.cloudcommerce.module.account.service.UserService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,25 +11,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-    private final UserCredentialsRepository userCredentialsRepository;
-    private final UserRepository userRepository;
+    private final UserService userService;
+    private final UserCredentialsService userCredentialsService;
 
-    public CustomUserDetailsService(
-        UserCredentialsRepository userCredentialsRepository,
-        UserRepository userRepository
-    ) {
-        this.userCredentialsRepository = userCredentialsRepository;
-        this.userRepository = userRepository;
+    public CustomUserDetailsService(UserService userService, UserCredentialsService userCredentialsService) {
+        this.userService = userService;
+        this.userCredentialsService = userCredentialsService;
     }
 
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        var user = userRepository.findByEmail(email)
-            .orElseThrow(() -> new NotFoundException("No user found with email " + email));
 
-        var userCredentials = userCredentialsRepository.findByEmail(email)
-            .orElseThrow(() -> new NotFoundException("No user found with email " + email));
+        var user = userService.findByEmail(email);
+        var userCredentials = userCredentialsService.findByEmail(email);
 
         return new User(
             userCredentials.getEmail(),
