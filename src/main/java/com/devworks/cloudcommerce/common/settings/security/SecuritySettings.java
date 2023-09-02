8fdef468acf197966f25c093fa2f925a261970d1
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-09-02 09:58:40 -0300

    change: resolve security chains

--- src/main/java/com/devworks/cloudcommerce/common/settings/security/SecuritySettings.java
+++ src/main/java/com/devworks/cloudcommerce/common/settings/security/SecuritySettings.java
@@ -1,11 +1,8 @@
 package com.devworks.cloudcommerce.common.settings.security;
 
-import com.devworks.cloudcommerce.common.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,20 +10,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 @Configuration
 public class SecuritySettings {
-    private final CustomUserDetailsService customUserDetailsService;
-
-    public SecuritySettings(CustomUserDetailsService customUserDetailsService) {
-        this.customUserDetailsService = customUserDetailsService;
-    }
-
-    @Bean
-    public AuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setUserDetailsService(customUserDetailsService);
-        return provider;
-    }
-
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
