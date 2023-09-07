9297b6be48ec445b4d573fa8ee4ec5a57c373035
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-09-07 00:54:27 -0300

    add custom jwt authentication provider, check credentials repository

--- src/main/java/com/devworks/cloudcommerce/common/security/provider/JwtAuthenticationProvider.java
+++ src/main/java/com/devworks/cloudcommerce/common/security/provider/JwtAuthenticationProvider.java
@@ -1,29 +1,20 @@
 package com.devworks.cloudcommerce.common.security.provider;
 
 import com.devworks.cloudcommerce.common.exceptions.BadRequestException;
-import com.devworks.cloudcommerce.common.exceptions.NotFoundException;
-import com.devworks.cloudcommerce.module.account.repository.UserCredentialsRepository;
-import com.devworks.cloudcommerce.module.account.repository.UserRepository;
+import com.devworks.cloudcommerce.module.account.service.UserCredentialsService;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtAuthenticationProvider implements AuthenticationProvider {
 
-    private final UserCredentialsRepository userCredentialsRepository;
-    private final UserRepository userRepository;
-    private final UserDetailsService userDetailsService;
+    private final UserCredentialsService userCredentialsService;
 
-    public JwtAuthenticationProvider(UserDetailsService userDetailsService,
-                                     UserCredentialsRepository userCredentialsRepository,
-                                     UserRepository userRepository) {
-        this.userDetailsService = userDetailsService;
-        this.userCredentialsRepository = userCredentialsRepository;
-        this.userRepository = userRepository;
+    public JwtAuthenticationProvider(UserCredentialsService userCredentialsService) {
+        this.userCredentialsService = userCredentialsService;
     }
 
     @Override
@@ -31,15 +22,12 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
         String email = authentication.getName();
         String password = String.valueOf(authentication.getCredentials());
 
-        System.out.println("003");
-        //Se eu fizer dessa forma, eu to acessando as informacoes em memorira,
-        var userDetails = userDetailsService.loadUserByUsername(email);
-//            var userDetails = userRepository.findByEmail(email)
-//                    .orElseThrow(() -> new NotFoundException("Nao xiste"));
-        System.out.println(userDetails.getUsername());
+        var userDetails = userCredentialsService.findByEmail(email);
 
         if(userDetails != null) {
-            return new UsernamePasswordAuthenticationToken(email, password, userDetails.getAuthorities());
+           return  new UsernamePasswordAuthenticationToken(
+               email, password, userDetails.getAuthorities()
+           );
         }
 
         throw new BadRequestException("Authentication Error");
