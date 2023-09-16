b6998069d9abbf5fd70d8d319cbaf81eb380301d
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-09-16 00:56:53 -0300

    populate action table with actionseed

---
+++ src/main/java/com/devworks/cloudcommerce/common/seed/ActionSeed.java
@@ -0,0 +1,37 @@
+package com.devworks.cloudcommerce.common.seed;
+
+import com.devworks.cloudcommerce.module.account.constants.ActionType;
+import com.devworks.cloudcommerce.module.account.model.Action;
+import com.devworks.cloudcommerce.module.account.repository.ActionRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class ActionSeed {
+    private final ActionRepository actionRepository;
+
+    public ActionSeed(ActionRepository actionRepository) {
+        this.actionRepository = actionRepository;
+    }
+
+    @PostConstruct
+    public void init() {
+        Set<Action> actions = new HashSet<>();
+
+        for (ActionType actionType : ActionType.values()) {
+            if (!actionRepository.existsByName(actionType.getName())) {
+                var a = new Action();
+                a.setCreatedAt(LocalDateTime.now());
+                a.setName(actionType.getName());
+                a.setActive(true);
+                actions.add(a);
+            }
+        }
+
+        actionRepository.saveAll(actions);
+    }
+}
