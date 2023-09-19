6d536ac1c9f2ee1853ac81a0c1e1542b3ee9a858
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-09-19 18:16:15 -0300

    resolve re-render filter execution

--- src/main/java/com/devworks/cloudcommerce/common/settings/ApplicationSettings.java
+++ src/main/java/com/devworks/cloudcommerce/common/settings/ApplicationSettings.java
@@ -1,5 +1,7 @@
 package com.devworks.cloudcommerce.common.settings;
 
+import com.devworks.cloudcommerce.common.security.filter.JwtTokenFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +13,13 @@ public class ApplicationSettings {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public FilterRegistrationBean<JwtTokenFilter> jwtTokenFilterRegistration(JwtTokenFilter filter) {
+        FilterRegistrationBean<JwtTokenFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setEnabled(false);
+        return registration;
     }
 
     @Bean
