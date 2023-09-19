be91e48d1ce76f9ad7055956955547b5eb86838b
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-09-19 18:19:32 -0300

    add boolean query to get role exiting

--- src/main/java/com/devworks/cloudcommerce/module/account/repository/RoleRepository.java
+++ src/main/java/com/devworks/cloudcommerce/module/account/repository/RoleRepository.java
@@ -10,4 +10,7 @@ import java.util.UUID;
 public interface RoleRepository extends JpaRepository<Role, UUID> {
     @Query("SELECT r FROM Role r WHERE LOWER(r.name) = LOWER(?1)")
     Optional<Role> findByName(String name);
+
+    @Query("SELECT CASE WHEN COUNT(r) > 0 THEN true ELSE false END FROM Role r WHERE LOWER(r.name) = LOWER(?1)")
+    boolean existsByName(String name);
 }
