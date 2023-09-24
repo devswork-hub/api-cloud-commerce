bd3d760a98aadcc74395819a83ac09a847590df5
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-09-23 23:21:48 -0300

    update module routing

--- src/main/java/com/devworks/cloudcommerce/module/account/routing/AccountModuleRouting.java
+++ src/main/java/com/devworks/cloudcommerce/module/account/routing/AccountModuleRouting.java
@@ -13,10 +13,14 @@ import org.springframework.stereotype.Component;
 public class AccountModuleRouting extends AbstractHttpConfigurer<AccountModuleRouting, HttpSecurity> {
     public void accountRoutingChains(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-            .requestMatchers(HttpMethod.POST, "/sign-up").permitAll()
+            .requestMatchers(HttpMethod.POST, "/register").permitAll()
             .requestMatchers(HttpMethod.POST, "/authenticate").permitAll()
 
-            .requestMatchers(HttpMethod.GET, UserRoute.BASE.getValue()).hasAnyAuthority(RolesType.ADMIN.getName(), RolesType.CUSTOMER.getName())
+            .requestMatchers(HttpMethod.GET, UserRoute.BASE.getValue())
+                .hasAnyAuthority(
+                    RolesType.ADMIN.getName(),
+                    RolesType.CUSTOMER.getName()
+                )
             .requestMatchers(HttpMethod.GET, UserRoute.ALL_CHILDREN.getValue()).authenticated()
             .requestMatchers(HttpMethod.POST, UserRoute.BASE.getValue()).permitAll()
             .requestMatchers(HttpMethod.DELETE, UserRoute.ALL_CHILDREN.getValue()).permitAll()
@@ -24,7 +28,13 @@ public class AccountModuleRouting extends AbstractHttpConfigurer<AccountModuleRo
             .requestMatchers(HttpMethod.POST, RoleRoute.BASE.getValue()).permitAll()
             .requestMatchers(HttpMethod.POST, RoleRoute.ALL_CHILDREN.getValue()).permitAll()
             .requestMatchers(HttpMethod.GET, RoleRoute.BASE.getValue()).permitAll()
-            .requestMatchers(HttpMethod.GET, RoleRoute.ALL_CHILDREN.getValue()).permitAll()
+
+            // retrieve single role by id
+            .requestMatchers(HttpMethod.GET, RoleRoute.ALL_CHILDREN.getValue())
+                .hasAnyAuthority(
+                    RolesType.MANAGER.getName(),
+                    RolesType.ADMIN.getName()
+                )
             .requestMatchers(HttpMethod.DELETE, RoleRoute.ALL_CHILDREN.getValue()).permitAll()
 
             .requestMatchers(HttpMethod.POST, ResourceRoute.BASE.getValue()).permitAll()
