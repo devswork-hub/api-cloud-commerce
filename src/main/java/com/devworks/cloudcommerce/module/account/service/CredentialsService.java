ede4cc6cde48750cd266b33694fc2d255e2e802a
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-09-30 23:24:47 -0300

    update create method in CredentialsService

--- src/main/java/com/devworks/cloudcommerce/module/account/service/CredentialsService.java
+++ src/main/java/com/devworks/cloudcommerce/module/account/service/CredentialsService.java
@@ -5,33 +5,24 @@ import com.devworks.cloudcommerce.common.exceptions.NotFoundException;
 import com.devworks.cloudcommerce.common.utils.PasswordUtils;
 import com.devworks.cloudcommerce.common.utils.Validator;
 import com.devworks.cloudcommerce.module.account.constants.AccountStatusType;
-import com.devworks.cloudcommerce.module.account.constants.RolesType;
 import com.devworks.cloudcommerce.module.account.dto.UserCredentialsDTO;
 import com.devworks.cloudcommerce.module.account.mapper.UserCredentialsMapper;
 import com.devworks.cloudcommerce.module.account.mapper.UserMapper;
-import com.devworks.cloudcommerce.module.account.model.Role;
 import com.devworks.cloudcommerce.module.account.model.User;
-import com.devworks.cloudcommerce.module.account.model.UserCredentials;
-import com.devworks.cloudcommerce.module.account.repository.RoleRepository;
+import com.devworks.cloudcommerce.module.account.model.Credentials;
 import com.devworks.cloudcommerce.module.account.repository.UserCredentialsRepository;
 import com.devworks.cloudcommerce.module.account.service.rule.UserCredentialsServiceRules;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-
 @Service
-public class UserCredentialsService implements UserCredentialsServiceRules {
-    private final RoleRepository roleRepository;
+public class CredentialsService implements UserCredentialsServiceRules {
     private final UserCredentialsRepository userCredentialsRepository;
     private final UserService userService;
 
-
-    public UserCredentialsService(
-        RoleRepository roleRepository,
+    public CredentialsService(
         UserCredentialsRepository userCredentialsRepository,
         UserService userService
     ) {
-        this.roleRepository = roleRepository;
         this.userCredentialsRepository = userCredentialsRepository;
         this.userService = userService;
     }
@@ -46,33 +37,10 @@ public class UserCredentialsService implements UserCredentialsServiceRules {
             throw new BadRequestException("Credentials with email already exists!");
 
         var user = userService.findById(input.getUserId());
-
-        var userCredentialsWithRoles = assignRoleToUserCredentials(UserCredentialsMapper.toEntity(input, UserMapper.toEntity(user)));
-        userCredentialsRepository.save(userCredentialsWithRoles);
+        userCredentialsRepository.save(UserCredentialsMapper.toEntity(input, UserMapper.toEntity(user)));
     }
 
-    private UserCredentials assignRoleToUserCredentials(UserCredentials credentials) {
-        var roles = credentials.getRoles();
-
-        if (roles == null) {
-            roles = new HashSet<>();
-            credentials.setRoles(roles);
-        }
-
-        Role customerRole = roleRepository.findByName(RolesType.CUSTOMER.getName())
-            .orElseGet(() -> { // Create customer role if it doesn't exist
-                Role defaultRole = new Role();
-                defaultRole.setName(RolesType.CUSTOMER.getName());
-                defaultRole.setDescription(RolesType.CUSTOMER.getDescription());
-                return roleRepository.save(defaultRole);
-            });
-
-        roles.add(customerRole);
-
-        return credentials;
-    }
-
-    public UserCredentials findByEmail(String email) {
+    public Credentials findByEmail(String email) {
         return userCredentialsRepository.findByEmail(email)
             .orElseThrow(() -> new NotFoundException("User with credential not found"));
     }
@@ -82,7 +50,7 @@ public class UserCredentialsService implements UserCredentialsServiceRules {
         userCredentialsRepository.save(UserCredentialsMapper.toEntity(input, user));
     }
 
-    public UserCredentials findByEmailAndPassword(String email, String password) {
+    public Credentials findByEmailAndPassword(String email, String password) {
         var userCredentials = userCredentialsRepository.findByEmail(email)
             .orElseThrow(() -> new NotFoundException("Not found user with email " + email));
 
