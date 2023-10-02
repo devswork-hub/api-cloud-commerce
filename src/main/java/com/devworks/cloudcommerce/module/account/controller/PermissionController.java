42b4b6ea6083e9eb4a36848ea6eafa5d26b1e3f2
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-10-02 01:07:03 -0300

    update permission controller and service

--- src/main/java/com/devworks/cloudcommerce/module/account/controller/PermissionController.java
+++ src/main/java/com/devworks/cloudcommerce/module/account/controller/PermissionController.java
@@ -1,6 +1,5 @@
 package com.devworks.cloudcommerce.module.account.controller;
 
-import com.devworks.cloudcommerce.module.account.dto.input.permission.AssignPermissionsInput;
 import com.devworks.cloudcommerce.module.account.dto.input.permission.CreateInput;
 import com.devworks.cloudcommerce.module.account.model.Permission;
 import com.devworks.cloudcommerce.module.account.service.PermissionService;
@@ -8,8 +7,6 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/permission")
@@ -23,15 +20,5 @@ public class PermissionController {
     @PostMapping
     public ResponseEntity<Permission> create(@Valid @RequestBody CreateInput input) {
         return ResponseEntity.status(HttpStatus.OK).body(permissionService.create(input));
-    }
-
-    @PostMapping("/{role_uuid}/resource/{resource_uuid}")
-    public ResponseEntity<Void> assignResourcesOfARole(
-        @PathVariable("role_uuid") UUID roleUUID,
-        @PathVariable("resource_uuid") UUID resourceUUID,
-        @RequestBody AssignPermissionsInput input
-    ) {
-        permissionService.assignPermissions(roleUUID, resourceUUID, input);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 }
--- src/main/java/com/devworks/cloudcommerce/module/account/service/PermissionService.java
+++ src/main/java/com/devworks/cloudcommerce/module/account/service/PermissionService.java
@@ -1,21 +1,10 @@
 package com.devworks.cloudcommerce.module.account.service;
 
 import com.devworks.cloudcommerce.common.exceptions.BadRequestException;
-import com.devworks.cloudcommerce.module.account.dto.input.permission.AssignPermissionsInput;
 import com.devworks.cloudcommerce.module.account.dto.input.permission.CreateInput;
-import com.devworks.cloudcommerce.module.account.mapper.RoleMapper;
-import com.devworks.cloudcommerce.module.account.model.Action;
-import com.devworks.cloudcommerce.module.account.model.Resource;
-import com.devworks.cloudcommerce.module.account.model.Role;
 import com.devworks.cloudcommerce.module.account.model.Permission;
 import com.devworks.cloudcommerce.module.account.repository.PermissionRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.UUID;
-
 
 @Service
 public class PermissionService {
@@ -48,34 +37,4 @@ public class PermissionService {
 
         return permissionRepository.save(permission);
     }
-
-
-//    @Transactional
-//    public void assignPermissions(UUID roleUUID, UUID resourceUUID, AssignPermissionsInput input) {
-//        var role = roleService.findById(roleUUID);
-//        var resource = resourceService.findById(resourceUUID);
-//        var validActions =  actionService.getValidActions(input.actions());
-//
-//        var associations = findPermissions(RoleMapper.toEntity(role), resource);
-//
-//        if (associations.isEmpty()) {
-//            var permissions = new HashSet<Permission>();
-//
-//            for (Action action : validActions) {
-//                var permission = new Permission();
-//                permission.setResource(resource);
-//                permission.setAction(action);
-//
-//                permissions.add(permission);
-//            }
-//            permissionRepository.saveAll(permissions);
-//        } else {
-//            throw new BadRequestException("Permissions already assigned");
-//        }
-//
-//    }
-
-//    public List<Permission> findPermissions(Action action, Resource resource) {
-//        return permissionRepository.findByActionAndResource(action, resource);
-//    }
 }
