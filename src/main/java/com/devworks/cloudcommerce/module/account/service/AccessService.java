f79530a27da6bc4e1681cb83c4cd505509fc50cf
deyvisonborges <deyvisonsilvaborges@gmail.com>
2023-10-03 22:37:27 -0300

    adjusts in access

--- src/main/java/com/devworks/cloudcommerce/module/account/service/AccessService.java
+++ src/main/java/com/devworks/cloudcommerce/module/account/service/AccessService.java
@@ -9,6 +9,7 @@ import com.devworks.cloudcommerce.module.account.repository.RoleRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class AccessService {
@@ -44,41 +45,54 @@ public class AccessService {
             }
         }
 
-        return modules;
+        return modules.stream().collect(Collectors.toMap(
+            module -> module.get("name").toString(),
+            module -> module,
+            (existingModule, newModule) -> {
+                // TODO
+                // Resolve casting
+                List<String> actions = (List<String>) existingModule.get("actions");
+                List<Resource> resources = (List<Resource>) existingModule.get("resources");
+
+                actions.addAll((List<String>) newModule.get("actions"));
+                resources.addAll((List<Resource>) newModule.get("resources"));
+                existingModule.put("actions", actions);
+                existingModule.put("resources", resources);
+                return existingModule;
+            }
+        )).values().stream().toList();
     }
 
     private void addPermissionToModules(Permission permission, List<Map<String, Object>> modules) {
         for (Module module : permission.getModules()) {
             String moduleName = module.getName();
             String actionName = permission.getAction().getName();
-            var resource = permission.getResource();
+            Resource resource = permission.getResource();
+
             Map<String, Object> existingModule;
 
+            List<Resource> resources = new ArrayList<>();
             List<String> actions = new ArrayList<>();
             actions.add(actionName);
+            resources.add(Resource.builder()
+                .name(resource.getName())
+                .active(resource.isActive())
+                .path(resource.getPath())
+                .build());
 
-            existingModule = createNewModule(moduleName, module, resource, actions);
+            existingModule = createNewModule(moduleName, module, resources, actions);
             modules.add(existingModule);
         }
     }
 
-    private Map<String, Object> createNewModule(String moduleName, Module module, Resource resource, List<String> actions) {
+    private Map<String, Object> createNewModule(String moduleName, Module module, List<Resource> resources, List<String> actions) {
         Map<String, Object> newModule = new LinkedHashMap<>();
         newModule.put("name", moduleName);
         newModule.put("active", module.isActive());
-        newModule.put("resource", createResource(resource));
+        newModule.put("resources", resources);
         newModule.put("actions", actions);
         return newModule;
     }
-
-    private Map<String, Object> createResource(Resource res) {
-        Map<String, Object> resource = new LinkedHashMap<>();
-        resource.put("name", res.getName());
-        resource.put("active", res.isActive());
-        resource.put("path", res.getPath());
-        return resource;
-    }
-
 
     public record AccessInput(UUID userUUID) {}
 }
